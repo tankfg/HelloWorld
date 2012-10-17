1525199b82d6d86a437472e6cf76e00760a1172b
tankfg <tankfg@qq.com>
2012-10-17 10:13:12 +0800

    add a project

---
+++ src/com/example/helloworld/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.helloworld;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+import com.phonegap.DroidGap;
+
+public class MainActivity extends DroidGap {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
